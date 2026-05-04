ac3b87cd7b2af6692c92466e551a77b64d3e7b5a
Lucas <lucas.r3001@gmail.com>
2026-05-04 18:52:53 +0200

    Create constructor.h

---
+++ constructor.h
@@ -0,0 +1,42 @@
+// Constructors run before main()
+
+/* Constructors are not in the c standard
+ * This implementation requires specific compilers:
+ *	gcc
+ *	clang
+ *	msvc
+ */
+
+/* normal function:
+ *	void function()
+ *	{
+ *		// do stuff
+ *	}
+ *
+ * as a constructor:
+ *	MAKE_CONSTRUCTOR(function)
+ *	{
+ *		// do stuff
+ *	}
+ */
+
+#if defined(__GNUC__) || defined(__clang__)
+
+	#define MAKE_CONSTRUCTOR(fn)					\
+		__attribute__((constructor))				\
+		static void fn()
+
+#elif defined(_MSC_VER)
+
+	#pragma section(".CRT$XCU", read)
+	#define MAKE_CONSTRUCTOR(fn)					\
+		static void fn();					\
+		__declspec(allocate(".CRT$XCU"))			\
+		void (*const fn##_constructor)() = fn;			\
+		static void fn()
+
+#else
+
+	#error unsupported compiler (use gcc, clang or msvc)
+
+#endif
