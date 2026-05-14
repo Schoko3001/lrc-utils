7ef7bfd6b80010a52a38e89cc773e3ea8b7d91d5
Lucas <lucas.r3001@gmail.com>
2026-05-14 14:46:30 +0200

    added pid functions

--- lrc_processControl.h
+++ lrc_processControl.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <stdio.h>
 
 
 #if defined(_WIN32)
-#include "windows.h"
+#include <windows.h>
+#include <tlhelp32.h>
+
+#define lrc_pid DWORD
 
 static inline int lrc_processClone() {
 	char path[MAX_PATH];
@@ -33,6 +37,33 @@ static inline int lrc_processClone() {
 	}
 }
 
+static inline lrc_pid getParentPID() {
+	DWORD currentPID = GetCurrentProcessId();
+	DWORD parentPID = 0;
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (snapshot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(PROCESSENTRY32);
+
+	if (Process32First(snapshot, &pe)) {
+		do {
+			if (pe.th32ProcessID == currentPID) {
+				parentPID = pe.th32ParentProcessID;
+				break;
+			}
+		} while (Process32Next(snapshot, &pe));
+	}
+
+	CloseHandle(snapshot);
+}
+
+static inline lrc_pid getPID() {
+	return GetCurrentProcessId();
+}
 
 
 #elif defined(__APPLE__)
@@ -43,6 +74,10 @@ static inline int lrc_processClone() {
 
 #elif defined(__linux__)
 #include <spawn.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define lrc_pid pid_t
 
 extern char **environ;
 
@@ -68,6 +103,13 @@ static inline int lrc_processClone() {
 	}
 }
 
+static inline lrc_pid lrc_getParentPID() {
+	return getppid();
+}
+
+static inline lrc_pid lrc_getPID() {
+	return getpid();
+}
 
 #else
 	#error UNKOWN/MISSING_OS_SPECIFIER
