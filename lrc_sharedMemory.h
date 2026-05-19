6457291afd2a1dac23b6bbc384c0e36346ed5d28
Lucas <lucas.r3001@gmail.com>
2026-05-19 12:05:39 +0200

    futex

--- lrc_sharedMemory.h
+++ lrc_sharedMemory.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdint.h>
 #include <stdio.h>
+#include <stdatomic.h>
+
 
 #if defined(_WIN32)
 	#include <windows.h>
@@ -11,10 +13,13 @@
 #elif defined(__ANDROID__)
 	#error ANDROID_IS_NOT_SUPPORTED
 #elif defined(__linux__)
+	#include <linux/futex.h>
+	#include <sys/syscall.h>
 	#include <sys/types.h>
 	#include <sys/mman.h>
 	#include <sys/stat.h>
 	#include <unistd.h>
+	#include <stdint.h>
 	#include <fcntl.h>
 	typedef int lrc_hndl;
 	#define lrc_hndl_invalid -1
@@ -184,4 +189,50 @@ static inline void lrc_sharedMemory_unmap(void* ptr, size_t size) {
 #elif defined(__linux__)
 	munmap(ptr, size);
 #endif
+}
+
+
+
+static inline void lrc_sleepWhile(_Atomic int* ptr, int expected) {
+	atomic_thread_fence(memory_order_seq_cst);
+	do {
+#if defined(_WIN32)
+		WaitOnAddress(
+			ptr,
+			&expected,
+			sizeof(int),
+			INFINITE
+		);
+#elif defined(__linux__)
+		syscall(
+			SYS_futex,
+			ptr,
+			FUTEX_WAIT,
+			expected,
+			NULL,
+			NULL,
+			0
+		);
+#endif
+	} while(atomic_load_explicit(ptr, memory_order_acquire) == expected);
+	atomic_thread_fence(memory_order_seq_cst);
+}
+
+
+static inline void lrc_tryWakeup(_Atomic int* ptr) {
+	atomic_thread_fence(memory_order_seq_cst);
+#if defined(_WIN32)
+	WakeByAddressAll(ptr);
+#elif defined(__linux__)
+	syscall(
+		SYS_futex,
+		ptr,
+		FUTEX_WAKE,
+		INT_MAX,
+		NULL,
+		NULL,
+		0,
+	);
+#endif
+	atomic_thread_fence(memory_order_seq_cst);
 }
